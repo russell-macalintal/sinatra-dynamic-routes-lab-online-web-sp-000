fb8eea5be95f6a233ee39a126061714778200405
Russell Macalintal <russell.macalintal@gmail.com>
2020-03-24 05:46:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    
+  end
 
+  get '/square/:number' do
+    
+  end
+
+  get '/say/:number/:phrase' do
+    
+  end
 end
